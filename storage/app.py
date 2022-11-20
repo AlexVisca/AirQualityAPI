fd3700b6be9271fd3e2097a85f0f47696404e195
Alex Visca <alexvisca@gmail.com>
2022-11-20 00:23:30 -0800

    database connection handler

--- storage/app.py
+++ storage/app.py
@@ -17,7 +17,7 @@ import json
 import time
 import yaml
 from connexion import NoContent
-from data.base import Base
+from data.base import Base, connect, create_temp, create_envr
 from data.readings import Temperature, Environment
 from datetime import datetime
 from os import environ
@@ -33,11 +33,11 @@ DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 # Environment config
 if 'TARGET_ENV' in environ and environ['TARGET_ENV'] == 'prod':
-    logging.info("ENV - Prod")
+    logging.info("ENV: Production")
     app_conf_file = '/config/app_conf.yml'
     log_conf_file = '/config/log_conf.yml'
 else:
-    logging.info("ENV - Dev")
+    logging.info("ENV: Development")
     app_conf_file = 'app_conf.yml'
     log_conf_file = 'log_conf.yml'
 
@@ -56,13 +56,14 @@ SERVER_HOST = app_config['server']['host']
 SERVER_PORT = app_config['server']['port']
 DATA_TOPIC = app_config['events']['topic']
 
+DB_USER = app_config['datastore']['username']
+DB_PASS = app_config['datastore']['password']
+DB_HOST = app_config['datastore']['host']
+DB_PORT = app_config['datastore']['port']
+DB_NAME = app_config['datastore']['db']
+
 DB_ENGINE = create_engine(
-    f"mysql+pymysql://" +
-    f"{app_config['datastore']['username']}:" +
-    f"{app_config['datastore']['password']}@" +
-    f"{app_config['datastore']['host']}:" +
-    f"{app_config['datastore']['port']}/" +
-    f"{app_config['datastore']['db']}"
+    f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
@@ -102,6 +103,8 @@ def get_environment(timestamp: str) -> list:
 
 # storage functions
 def temperature(body) -> None:
+    logger.info(f"Received temperature telemetry -- trace ID: {body['trace_id']}")
+    
     session = DB_SESSION()
     temp = Temperature(
         body['device_id'], 
@@ -114,10 +117,12 @@ def temperature(body) -> None:
     session.commit()
 
     session.close()
-    logger.info(f"Received temperature telemetry -- trace ID: {body['trace_id']}")
+    
     return NoContent, 201
 
 def environment(body) -> None:
+    logger.info(f"Received environment telemetry -- trace ID: {body['trace_id']}")
+
     session = DB_SESSION()
     envr = Environment(
         body['device_id'],
@@ -131,12 +136,12 @@ def environment(body) -> None:
     session.commit()
 
     session.close()
-    logger.info(f"Received environment telemetry -- trace ID: {body['trace_id']}")
+    
     return NoContent, 201
 
 # message processor
 def process_messages():
-    topic = create_kafka_connection(max_retries=3, timeout=2)
+    topic = connect_kafka_client(max_retries=3, timeout=2)
 
     consumer = topic.get_simple_consumer(
         consumer_group=b'telemetry', 
@@ -162,8 +167,8 @@ def process_messages():
         consumer.stop()
         consumer.start()
 
-
-def create_kafka_connection(max_retries: int, timeout: int):
+# Server connection
+def connect_kafka_client(max_retries: int, timeout: int):
     count = 0
     while count < max_retries:
         try:
@@ -182,12 +187,55 @@ def create_kafka_connection(max_retries: int, timeout: int):
         logger.error(f"Connection failed - Unable to connect to kafka server. Max retries exceeded ({max_retries})")
         raise SystemExit(1)
 
+# Database utilities
+def init_db(database, connection, cursor):
+    try:
+        cursor.execute('''SHOW VARIABLES like 'version';''')
+        version = cursor.fetchone()
+        logger.info(f"Connected to {database} database - MySQL {version[0]} {version[1]}")
+        # 
+    except Exception as e:
+        raise
+
+def _tables(database, connection, cursor):
+    cursor.execute('''SHOW TABLES;''')
+    tables = cursor.fetchall()
+    try:
+        temp = tables[-1]
+        envr = tables[-2]
+        logger.info(f"{database} tables: {temp[0]}, {envr[0]}")
+        # 
+    except IndexError:
+        logger.info(f"Creating tables")
+        cursor.execute(create_temp)
+        logger.info("Created table `temperature`")
+        cursor.execute(create_envr)
+        logger.info("Created table `environment`")
+        connection.commit()
+        # 
+    except Exception as e:
+        raise
+
+# Database connection
+def connect_database(user: str, password: str, host: str, port: int, database: str):
+    with connect(user=user, password=password, host=host, port=port, database=database, auth_plugin='caching_sha2_password') as cnx:
+        crs = cnx.cursor()
+        try:
+            init_db(database, cnx, crs)
+            _tables(database, cnx, crs)
+            # 
+        except Exception as e:
+            logger.error(str(e))
+
+        finally:
+            crs.close()
 
 
 app = connexion.FlaskApp(__name__, specification_dir='openapi/')
 app.add_api('openapi.yml', strict_validation=True, validate_responses=True)
 
 def main() -> None:
+    connect_database(user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT, database=DB_NAME)
     t1 = Thread(target=process_messages)
     t1.daemon
     t1.start()
--- storage/data/base.py
+++ storage/data/base.py
@@ -1,3 +1,56 @@
+import mysql.connector
+from contextlib import contextmanager
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+@contextmanager
+def connect(**kwargs):
+    connection = mysql.connector.connect(**kwargs)
+    try:
+        yield connection
+
+    finally:
+        connection.close()
+
+
+create_temp = '''
+    CREATE TABLE temperature
+    (id_ INT NOT NULL AUTO_INCREMENT,
+    date_created VARCHAR(100) NOT NULL,
+    device_id VARCHAR(250) NOT NULL,
+    location VARCHAR(250) NOT NULL,
+    temperature DECIMAL(5,2) NOT NULL,
+    timestamp VARCHAR(100) NOT NULL,
+    trace_id VARCHAR(250) NOT NULL,
+    CONSTRAINT temperature_pk PRIMARY KEY (id_))
+    '''
+
+create_envr = '''
+    CREATE TABLE environment
+    (id_ INT NOT NULL AUTO_INCREMENT,
+    date_created VARCHAR(100) NOT NULL,
+    device_id VARCHAR(250) NOT NULL,
+    location VARCHAR(250) NOT NULL,
+    pm2_5 INTEGER NOT NULL,
+    co_2 INTEGER NOT NULL,
+    timestamp VARCHAR(100) NOT NULL,
+    trace_id VARCHAR(250) NOT NULL,
+    CONSTRAINT environment_pk PRIMARY KEY (id_))
+    '''
+
+empty_temp = '''
+    TRUNCATE TABLE temperature;
+    '''
+
+empty_envr = '''
+    TRUNCATE TABLE environment;
+    '''
+
+drop_all = '''
+    DROP TABLE temperature, environment;
+    '''
+
+version = '''
+    SHOW VARIABLES like 'version';
+    '''
