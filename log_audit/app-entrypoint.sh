da925389152129536522627f9a88a04066495685
Alex Visca <alexvisca@gmail.com>
2022-11-25 14:22:59 -0800

    refactor

--- log_audit/app-entrypoint.sh
+++ log_audit/app-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 # wait for the server to be up
 if [ -n "$SERVER_HOST" ]; then
-    /usr/bin/wait-for-it "$SERVER_HOST:${SERVER_PORT:-9092}"
+    /usr/bin/wait-for-it "$SERVER_HOST:${SERVER_PORT:-9092}" -t 0
 fi
 # run the main container command
 exec "$@"
