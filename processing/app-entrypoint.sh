4fb69c023a017acaac46afeebb9e6f598062466c
Alex Visca <alexvisca@gmail.com>
2022-11-26 09:48:57 -0800

    entrypoint update

--- processing/app-entrypoint.sh
+++ processing/app-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 # wait for the server to be up
 if [ -n "$SERVER_HOST" ]; then
-    /usr/bin/wait-for-it "$SERVER_HOST:${SERVER_PORT:-80}" -t 120
+    /usr/bin/wait-for-it "$SERVER_HOST:${SERVER_PORT:-80}/storage" -t 120
 fi
 # run the main container command
 exec "$@"
